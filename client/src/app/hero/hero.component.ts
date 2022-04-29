3683eb2fea1618f0ea918ecb9dd4503b7889dd1a
Hpeled <74018137+Hpeled@users.noreply.github.com>
2022-04-29 12:55:35 +0300

    heroes

---
+++ client/src/app/hero/hero.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../heroes/hero';
+
+@Component({
+  selector: 'app-hero',
+  templateUrl: './hero.component.html',
+  styleUrls: ['./hero.component.css'],
+})
+export class HeroComponent implements OnInit {
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm',
+  };
+  constructor() {}
+
+  ngOnInit(): void {}
+}
--- client/src/app/heroes/heroes.component.ts
+++ client/src/app/heroes/heroes.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Hero } from './hero';
+import { HEROES } from './heroes';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
+  heroes = HEROES;
+  selectedHero?: Hero;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
